7186eb6f25c23a733f318f7b2745f02877871264
Zach Carrera <zachcarrera@gmail.com>
2023-04-10 14:44:13 -0700

    add test directory

---
+++ src/test/java/com/zachcarrera/hoppersreceipt/HoppersReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachcarrera.hoppersreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
